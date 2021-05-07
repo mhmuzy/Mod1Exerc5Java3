e3c1f5cd9b73a2d97e79204dd8e5a0e6a387ee13
marcio henrique <mhmuzy@embratel.com.br>
2021-05-07 13:10:57 -0300

    Atendimento do Chamado [Treinamento]

---
+++ src/Hitss/Treinamento/Infra/Controller.java
@@ -0,0 +1,55 @@
+package Hitss.Treinamento.Infra;
+
+public class Controller {
+	
+	public static void Play() {
+		
+		String[] Sexo = new String[4];
+		Sexo[1] = "M";
+		Sexo[2] = "F";
+		Sexo[3] = "F";
+		
+		String[] Regiao = new String[4];
+		Regiao[1] = "RJ";
+		Regiao[2] = "SP";
+		Regiao[3] = "Maranhão";
+		
+		for (int i = 1; i < Regiao.length; i++) {
+			Listagem(Regiao[i], Sexo[i]);
+		}
+		
+	}
+	
+	public static void Listagem(String regiao, String sexo) {
+		
+		if (regiao == "RJ") {
+			
+			System.out.println("Região " + regiao + ":");
+			if (sexo == "M") {
+				System.out.println("Sexo Masculino.");	
+			} else {
+				System.out.println("Sexo Feminino.");	
+			}
+		} else if (regiao == "SP") {
+			System.out.println("Região " + regiao + ":");
+			if (sexo == "M") {
+				System.out.println("Sexo Masculino.");	
+			} else {
+				System.out.println("Sexo Feminino.");	
+			}
+		} else {
+			System.out.println("Região " + regiao + ":");
+			if (sexo == "M") {
+				System.out.println("Sexo Masculino.");	
+			} else {
+				System.out.println("Sexo Feminino.");	
+			}
+		}
+		System.out.println("Região " + regiao + ":");
+		if (sexo == "M") {
+			System.out.println("Sexo Masculino.");	
+		} else {
+			System.out.println("Sexo Feminino.");	
+		}	
+	}	
+}
